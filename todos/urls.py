1920311975905778a8eaecc41c99ef163defe364
basekeet <alexandr.arhipochkin@gmail.com>
2021-11-03 12:20:21 +0300

    Added minimal views

--- todos/urls.py
+++ todos/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'todos'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:todolist_id>/', views.detail, name='detail'),
+    path('<int:todolist_id>/<int:todo_id>/', views.todo, name='todo')
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,21 @@
-from django.http import HttpResponse
+from django import template
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, response
+from django.template import loader
+from django.http import Http404
+
+from todos.models import TodoList, Todo
 
 def index(request):
-    return HttpResponse("Hello")
+    todolists_list = TodoList.objects.all()
+    context = {
+        "todolists_list": todolists_list
+    }
+    return render(request, 'todos/index.html', context)
+
+def detail(request, todolist_id):
+    todos_list = get_object_or_404(TodoList, pk=todolist_id)
+    return render(request, 'todos/detail.html', {"todos_list":todos_list.todo_set.all(), "todolist_id": todolist_id})
+
+def todo(requse, todolist_id, todo_id):
+    return HttpResponse(Todo.objects.get(pk=todo_id))
